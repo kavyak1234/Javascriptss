a783c8b0b5788fd4052026dfe58f1b6348a04e42
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-03 13:17:08 +0530

    initial command

---
+++ java/datatypes.js
@@ -0,0 +1,28 @@
+// 2 types datatypes 
+// 1.primative datatypes
+// 2.non primative datatypes
+// 1.primative datatypes-1.string,2.Number,3.boolean,4.undefined
+// 1.string
+let name ="kavya kannan"
+console.log(name)
+console.log(typeof name)
+
+// 2.Number
+let number = 7034064303
+console.log(number)
+console.log(typeof number)
+
+// 3.boolean
+let boolean = true
+console.log(boolean)
+console.log(typeof boolean)
+
+// 4.undefined
+let age;
+console.log(age)
+console.log(typeof age)
+
+// 5.null
+let sing=null
+console.log(sing)
+console.log(typeof sing)
