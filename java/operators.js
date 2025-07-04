b262b5e98a96cae3e389fdaad4395d521af5d9d7
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-04 12:59:29 +0530

    initial command

--- java/operators.js
+++ java/operators.js
@@ -1,49 +1,80 @@
 // / ARITHAMETIC OPERATORS
 
-let number1 = 77;
-let number2 = 67;
+// let number1 = 77;
+// let number2 = 67;
 
-// ADDITION
+// // ADDITION
 
-let sum = number1 + number2;
+// let sum = number1 + number2;
 
-console.log(sum);
+// console.log(sum);
 
-// SUBTRACTION
+// // SUBTRACTION
 
-let difference = number1 - number2;
+// let difference = number1 - number2;
 
-console.log(difference);
+// console.log(difference);
 
-// MULTIPLICATION
+// // MULTIPLICATION
 
-let product = number1 * number2;
+// let product = number1 * number2;
 
-console.log(product);
+// console.log(product);
 
-// DIVISION
+// // DIVISION
 
-let quotient = number1 / number2;
+// let quotient = number1 / number2;
 
-console.log(quotient);
+// console.log(quotient);
 
-// MODULUS
+// // MODULUS
 
-let reminder = number1 % number2;
+// let reminder = number1 % number2;
 
-console.log(reminder);
+// console.log(reminder);
 
-// increment operator
+// // increment operator
 
-let num3 = 10;
-num3++;
-console.log(num3, "after increment");
+// let num3 = 10;
+// num3++;
+// console.log(num3, "after increment");
 
-let num4 = 3;
+// let num4 = 3;
 
-num4--;
-console.log(num4, "after decrement");
+// num4--;
+// console.log(num4, "after decrement");
 
-num4--
+// num4--
 
-console.log(num4, "after decrement");
+// console.log(num4, "after decrement");
+
+
+// let a = 20;
+// let b = 20;
+
+// // > GREATER THAN OPERATOR
+// // < LESS THAN OPERATOR
+
+// // console.log(a < 0);
+// // comparisson operators
+// // >= GREATER THAN OR EQUAL TO OPERATOR
+// console.log(a >= b);
+// // comparison operators
+// // <= LESS THAN OR EQUAL TO OPERATOR
+// console.log(a <= b);
+
+// // LOGICAL AND OPERATOR -----&&
+
+// console.log(a >= b && a == 20);
+
+// // LOGICAL OR OPERATOR -----
+
+// console.log(a > b || a == 20);
+
+// let value3=20
+// let value4="asdfghjkl";
+// console.log(value3==value4,'comparison 20 and 20')
+
+// console.log(a >= b && a==20);
+// console.log(a > b || a==20);
+
---
+++ java/question.js
@@ -0,0 +1,10 @@
+// 1.question1
+function checkEvenOrodd(num1,num2) {
+    const sum = num1 + num2
+    return sum % 2 ===0 ? "this number is even":"this number is odd";
+}
+console.log(checkEvenOrodd(3,5));
+console.log(checkEvenOrodd(2,5));
+
+// 2.question2
+funct
