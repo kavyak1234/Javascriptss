c317ad4742dbf4a2c63eb4ba33d46b30644b5c41
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-10 12:56:37 +0530

    initial command

--- java/lexicalscope.js
+++ java/lexicalscope.js
@@ -40,33 +40,59 @@
 
 
 
-// questions
-function createAdder(x) {
-    return function(y) {
-        return x+y;
-    }
+// // questions
+// function createAdder(x) {
+//     return function(y) {
+//         return x+y;
+//     }
 
-}
-const add5 = createAdder(5);
-console.log(add5(10)); 
+// }
+// const add5 = createAdder(5);
+// console.log(add5(10)); 
 
-// 4. 
-function greet(name) {
-  return function() {
-    console.log(`Hello ${name}`);
+// // 4. 
+// function greet(name) {
+//   return function() {
+//     console.log(`Hello ${name}`);
+//   }
+// }
+
+
+// const greetKavya = greet("Kavya");
+// greetKavya(); 
+
+// // 5.
+// function outer() {
+//     let x =5;
+//     return function inner() {
+//         console.log(x);
+//     }
+// }
+// const fn = outer();
+// fn();
+
+// 3.
+function counter() {
+  let count = 0;
+
+  function increment() {
+    count++;
+    return count;
   }
+
+  function decrement() {
+    count--;
+    return count;
+  }
+
+  return { increment, decrement };
 }
 
 
-const greetKavya = greet("Kavya");
-greetKavya(); 
-
-// 5.
-function outer() {
-    let x =5;
-    return function inner() {
-        console.log(x);
-    }
-}
-const fn = outer();
-fn();
+const c = counter();
+console.log(c.increment()); 
+console.log(c.increment());
+console.log(c.decrement());
+console.log(c.decrement());
+console.log(c.increment());
+console.log(c.increment());
