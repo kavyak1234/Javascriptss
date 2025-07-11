81dd09a10590448c620f2fb9c0a2ee8f0b08c8ec
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-11 13:23:01 +0530

    initial command

--- java/scripts.js
+++ java/scripts.js
@@ -3,6 +3,11 @@ console.log(totalLength);
 document.getElementsByTagName("h1")[totalLength -1].innerHTML =
 "<u>kavya kannan</u>";
 document.getElementsByTagName("button")[0].style.backgroundColor = "blue";
-document.getElementsByTagName("button")[0].style.color = "white";
+document.getElementsByTagName("button")[0].style.color = "red";
 document.getElementsByTagName("button")[0].style.padding = "10px";
 document.getElementsByTagName("button")[0].style.border = "none";
+
+document.getElementById("heading_1").style.backgroundColor = "yellow";
+document.getElementsByClassName("heading_element")[1].style.backgroundColor ="blue";
+document.getElementsByClassName("heading_element")[2].style.backgroundColor ="blue";
+console.log(document.getElementsByClassName("heading_element"));
