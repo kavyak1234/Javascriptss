0f6e03c3d585b802441642f3be75c6b58a9da186
kavyak1234 <kavyakkavya153@gmail.com>
2025-07-14 13:30:32 +0530

    initial command

--- java/scripts.js
+++ java/scripts.js
@@ -1,13 +1,13 @@
-const totalLength = document.getElementsByTagName("h1") .length;
-console.log(totalLength);
-document.getElementsByTagName("h1")[totalLength -1].innerHTML =
-"<u>kavya kannan</u>";
-document.getElementsByTagName("button")[0].style.backgroundColor = "blue";
-document.getElementsByTagName("button")[0].style.color = "red";
-document.getElementsByTagName("button")[0].style.padding = "10px";
-document.getElementsByTagName("button")[0].style.border = "none";
+// const totalLength = document.getElementsByTagName("h1") .length;
+// console.log(totalLength);
+// document.getElementsByTagName("h1")[totalLength -1].innerHTML =
+// "<u>kavya kannan</u>";
+// document.getElementsByTagName("button")[0].style.backgroundColor = "blue";
+// document.getElementsByTagName("button")[0].style.color = "red";
+// document.getElementsByTagName("button")[0].style.padding = "10px";
+// document.getElementsByTagName("button")[0].style.border = "none";
 
-document.getElementById("heading_1").style.backgroundColor = "yellow";
-document.getElementsByClassName("heading_element")[1].style.backgroundColor ="blue";
-document.getElementsByClassName("heading_element")[2].style.backgroundColor ="blue";
-console.log(document.getElementsByClassName("heading_element"));
+// document.getElementById("heading_1").style.backgroundColor = "yellow";
+// document.getElementsByClassName("heading_element")[1].style.backgroundColor ="blue";
+// document.getElementsByClassName("heading_element")[2].style.backgroundColor ="blue";
+// console.log(document.getElementsByClassName("heading_element"));
